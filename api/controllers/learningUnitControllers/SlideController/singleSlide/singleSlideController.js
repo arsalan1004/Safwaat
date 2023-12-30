ede13e9f120d04c5e48722f6fb24e4deb6f43521
arsalan1004 <syedarsalan3505@gmail.com>
2023-12-30 22:37:23 +0500

    feat: single slide API

---
+++ api/controllers/learningUnitControllers/SlideController/singleSlide/singleSlideController.js
@@ -0,0 +1,35 @@
+const {
+  practiceSlideModel,
+} = require("../../../../models/learningUnitModels/SlidesModel/practiceSlideModel");
+const {
+  teachingSlideModel,
+} = require("../../../../models/learningUnitModels/SlidesModel/teachingSlideModel");
+
+const getSlide = async (req, res) => {
+  try {
+    const teachingSlide = await teachingSlideModel.findOne({
+      _id: req.params.slideId,
+    });
+    const practiceSlide = await practiceSlideModel.findOne({
+      _id: req.params.slideId,
+    });
+
+    if (teachingSlide) {
+      res.status(200).json(teachingSlide);
+    } else if (practiceSlide) {
+      res.status(200).json(practiceSlide);
+    } else {
+      console.log(`Error in finding slide in getSlide controller`);
+      res.status(400).json("No slide found");
+    }
+  } catch (error) {
+    console.log(
+      `Error occured at getSlide controller on single slide route:${error}`
+    );
+    res.status(400).json({ message: "Could not get slide" });
+  }
+};
+
+module.exports = {
+  getSlide,
+};
--- api/routes/learningUnitRoutes/SlideRoute/slideRoute.js
+++ api/routes/learningUnitRoutes/SlideRoute/slideRoute.js
@@ -1,9 +1,13 @@
 const {
   getAllSlides,
 } = require("../../../controllers/learningUnitControllers/SlideController/allSlides/allSlideController");
+const {
+  getSlide,
+} = require("../../../controllers/learningUnitControllers/SlideController/singleSlide/singleSlideController");
 
 const slideRoute = require("express").Router();
 
 slideRoute.get("/:unitId", getAllSlides);
+slideRoute.get("/single/:slideId", getSlide);
 
 module.exports = { slideRoute };

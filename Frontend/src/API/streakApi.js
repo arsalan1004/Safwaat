233fe65ea193648221802ee995b067d218f95644
Muhammad Zain <muhammadzain0742@gmail.com>
2024-01-04 21:44:15 +0500

    refactor: updated comments

--- Frontend/src/API/streakApi.js
+++ Frontend/src/API/streakApi.js
@@ -18,22 +18,23 @@ const getStreakRange = async (month, year) => {
   
   // `${API_URL}/streak/calendarData?id=${id}&month=${month}&year=${year}`
   try {
-    // let response = await axios.get(API_URL)
-    // console.log(response)
+
 
     let response = await axios.post(API_URL, {
       id: "65901f9ae137a46acf78c715",
       month: month,
       year: year
     });
-      console.log(response)
+
+    console.log(`In streakApi: ${response}`)
     return response.data
+
   } catch (error) {
-    // console.log(error)
+    console.log(error)
   }
   
   
-  // return ({start: 3, end: 13, streakCount: 13-3});
+
 }
 
 export {getStreakRange}

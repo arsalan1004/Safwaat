3727894fb38be5193313472aad7d111465f1604f
MZubair1512 <mzubair15121716@gmail.com>
2024-01-14 23:57:42 +0500

    Front Changes

--- Frontend/src/Components/Login_Signup/login.jsx
+++ Frontend/src/Components/Login_Signup/login.jsx
@@ -8,7 +8,7 @@ const Login = () => {
     <div className="w-screen h-screen wrapper flex items-center">
       <div className="box w-screen h-[80vh]">
             <div className="absolute flex">
-                <p className='max-[600px]:hidden type-log mx-[5vw] my-[6.4vh]'><span className='type-logHead my-[10vh]'>WELCOME BACK !</span><br></br> BEST TIME TO RESUME <br /> YOUR <span className='italic font-thin'>TAJWEED</span> JOURNEY <br /> WITH US</p> 
+                <p className='max-[600px]:hidden type-log mx-[5vw] my-[6.4vh]'><span className='type-logHead my-[10vh]'>WELCOME BACK !</span><br /><br /> BEST TIME TO RESUME <br /> YOUR <span className='italic font-thin'>TAJWEED</span> JOURNEY <br /> WITH US</p> 
                 <LoginDialog/>
             </div>
         </div>

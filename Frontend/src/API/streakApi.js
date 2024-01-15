c08ecd875931c6a16f4f1996cd5be32fcf6f04b1
Khawar Khan <ma3533899@gmail.com>
2024-01-15 12:30:20 +0500

    feat: Tested Streak Calendar

--- Frontend/src/API/streakApi.js
+++ Frontend/src/API/streakApi.js
@@ -13,7 +13,7 @@ const getStreakRange = async (month, year) => {
   console.log(`month: ${month}`);
   console.log(`year: ${year}`);
   
-    const id =  "65901f9ae137a46acf78c715";
+    const id =  "659815525ce38b434230fbe0";
 
   
   // `${API_URL}/streak/calendarData?id=${id}&month=${month}&year=${year}`
@@ -21,7 +21,7 @@ const getStreakRange = async (month, year) => {
 
 
     let response = await axios.post(API_URL, {
-      id: "65901f9ae137a46acf78c715",
+      id: "659815525ce38b434230fbe0",
       month: month,
       year: year
     });

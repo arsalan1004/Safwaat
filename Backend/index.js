6f1f4ed47c288c190eb9703afb12b14af4fa1899
Khawar Khan <ma3533899@gmail.com>
2024-01-15 11:52:01 +0500

    feat: Modified index.js according to main

--- Backend/index.js
+++ Backend/index.js
@@ -1,18 +1,46 @@
-const cors = require('cors');
-const bodyParser = require('body-parser');
+
+const http = require("http");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const {
+  flashCardSetRouter,
+} = require("./api/routes/FCSetRoute/flashCardSetRoute");
+const { flashCardRouter } = require("./api/routes/FCRoute/flashCardRoute");
 const streakLeaderboardRoute = require('./api/routes/streakLeaderboardRoute/streakLeaderboard');
 const xpLeaderboardRoute = require('./api/routes/xpLeaderboardRoute/xpLeaderboardRoute');
-const express = require('express');
+require("dotenv").config();
 
+// IMPORTING .ENV VARIABLE
+const port = process.env.PORT || 8000;
+
+// CONNECTING TO DATABASE
+require("./config/db");
+
+// CREATING APP
 const app = express();
-
-require('./config/db');
-require('dotenv').config();
-app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 app.use(bodyParser.json());
-let port = process.env.PORT || 8000;
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 
+// REGISTERING ROUTES
+app.use("/api/FlashCraft", flashCardSetRouter);
+app.use("/api/flashcard", flashCardRouter);
 app.use("/xpleaderboard", xpLeaderboardRoute);
 app.use("/streakLeaderboard", streakLeaderboardRoute);
 
-app.listen(port, () => console.log('Runnning on  http://localhost:8000'));
+// CREATING SERVER
+const server = http.createServer(app);
+
+//LISTENING THE SERVER
+server.listen(port, () => console.log(`Server listening on port ${port}`));

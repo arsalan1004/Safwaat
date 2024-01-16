8f6513380673ec99de73820a1da9ce615934f4e4
Khawar Khan <ma3533899@gmail.com>
2024-01-17 01:37:20 +0500

    feat: Added userAnalytics Route

---
+++ Backend/api/routes/UserAnalyticsRoute/userAnalyticsRoute.js
@@ -0,0 +1,14 @@
+const Router = require('express').Router();
+const {
+    getUserAnalytics,
+    AnalyticsForAll,
+    updateAnalytics,
+    schedularDailyTaskForAnalytics,
+    schedularMonthlyHandler,
+    schedularWeeklyHandler
+} = require('./../../controllers/UserAnalyticsController/userAnalyticsController');
+
+Router.get("/get/:userId",getUserAnalytics);
+Router.post("/assignAll", AnalyticsForAll);
+
+module.exports = Router;

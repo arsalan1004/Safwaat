90569817dbe0cbdd926c07f8f85d00bcac05ff92
MZubair1512 <mzubair15121716@gmail.com>
2024-01-16 14:19:03 +0500

    Minor Changes

--- Frontend/src/Components/LeaderBoard/leaderboad.jsx
+++ Frontend/src/Components/LeaderBoard/leaderboad.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import logo from '../../Assets/login.jpg';
 import leaderboard from '../../Assets/bronze.png';
+import bronze from '../../Assets/bronze.png';
 import purple from '../../Assets/purple.png'
 import gold from '../../Assets/gold.png'
 import red from '../../Assets/red.png'
